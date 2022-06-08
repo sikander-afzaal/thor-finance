29a4cddbfdf670ae5c8f2c5ab0302f255f568377
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-06-09 01:10:49 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Layout/Header/Header";
+import Hero from "./Pages/Home/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header />
+        <Hero />
       </div>
     </div>
   );
---
+++ src/Pages/Home/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Hero.css";
+import vid from "../../../Assets/hero.mp4";
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="top-overlay"></div>
+      <video src={vid} muted autoPlay controls={false} loop />
+      <div className="bottom-overlay"></div>
+    </div>
+  );
+}
+
+export default Hero;
